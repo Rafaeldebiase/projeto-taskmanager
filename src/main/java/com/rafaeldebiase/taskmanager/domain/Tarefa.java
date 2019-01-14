f87dbbe174f8d2730b41d229ff6604aa67542d80
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-01-14 04:21:43 -0200

    hotfix tarefa.java

--- src/main/java/com/rafaeldebiase/taskmanager/domain/Tarefa.java
+++ src/main/java/com/rafaeldebiase/taskmanager/domain/Tarefa.java
@@ -1,5 +1,126 @@
 package com.rafaeldebiase.taskmanager.domain;
 
-public class Tarefa {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+/**
+ * @author Rafael de Biase
+ *
+ */
+
+@Entity
+public class Tarefa {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private Boolean concluido;
+	
+	public Tarefa() {
+	}
+
+	/**
+	 * @param id
+	 * @param nome
+	 * @param descricao
+	 * @param concluido
+	 */
+	public Tarefa(Integer id, String nome, String descricao, Boolean concluido) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.concluido = concluido;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the descricao
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+
+	/**
+	 * @param descricao the descricao to set
+	 */
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	/**
+	 * @return the concluido
+	 */
+	public Boolean getConcluido() {
+		return concluido;
+	}
+
+	/**
+	 * @param concluido the concluido to set
+	 */
+	public void setConcluido(Boolean concluido) {
+		this.concluido = concluido;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarefa other = (Tarefa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
