dcf7a9f5b6030b10add3443716c56a7cb7ca79dc
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-01-14 12:30:56 -0200

    cria enum tipousuario

---
+++ src/main/java/com/rafaeldebiase/taskmanager/domain/enums/PerfilUsuario.java
@@ -0,0 +1,39 @@
+package com.rafaeldebiase.taskmanager.domain.enums;
+
+public enum PerfilUsuario {
+	
+	ADMIN(1, "ROLE_ADMIN"),
+	USUARIO(1, "ROLE_USUARIO");
+	
+	private int cod;
+	private String descricao;
+	
+	private PerfilUsuario(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static PerfilUsuario toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+		
+		for (PerfilUsuario x: PerfilUsuario.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+	
+
+}
