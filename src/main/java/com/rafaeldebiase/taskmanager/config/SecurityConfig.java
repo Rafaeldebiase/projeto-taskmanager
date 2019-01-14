4dbb046be229509381c5cd02f98d53ba675a62b3
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-01-14 05:03:49 -0200

    configura JWT

---
+++ src/main/java/com/rafaeldebiase/taskmanager/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.rafaeldebiase.taskmanager.config;
+
+import java.util.Arrays;
+
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+/**
+ * @author Rafael de Biase
+ *
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+		
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**",
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/tarefas/**"
+	};
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable();
+		http.authorizeRequests()
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
+		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+		.anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean
+	UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+}
