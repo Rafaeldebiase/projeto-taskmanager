b65fe14caa634134795acd2cbff10c50f06f8d14
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-01-14 05:36:00 -0200

    usuario resource

---
+++ src/main/java/com/rafaeldebiase/taskmanager/resource/UsuarioResource.java
@@ -0,0 +1,64 @@
+package com.rafaeldebiase.taskmanager.resource;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.rafaeldebiase.taskmanager.domain.Usuario;
+import com.rafaeldebiase.taskmanager.service.UsuarioService;
+
+
+@RestController
+@RequestMapping(value="/usuarios")
+public class UsuarioResource {
+	
+	@Autowired
+	private UsuarioService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Usuario obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<Usuario>> page(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPege", defaultValue="24") Integer linesPerPege, 
+			@RequestParam(value="oderBy", defaultValue="nome") String oderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Usuario> list = service.findPage(page, linesPerPege, oderBy, direction);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Usuario obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Usuario obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
