4cea01e8f9ab37f468f3377f15f424d92227ddcf
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-01-14 23:12:41 -0200

    hot fix

--- src/main/java/com/rafaeldebiase/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/rafaeldebiase/taskmanager/config/SecurityConfig.java
@@ -3,6 +3,8 @@
  */
 package com.rafaeldebiase.taskmanager.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -70,8 +72,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Bean
 	UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
 	
