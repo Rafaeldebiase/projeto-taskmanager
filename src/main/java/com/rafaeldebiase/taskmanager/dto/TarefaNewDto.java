47dd226806be930b9ab97fcce90928572fe2981d
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-01-20 20:20:56 -0200

    cria tarefa new dto

---
+++ src/main/java/com/rafaeldebiase/taskmanager/dto/TarefaNewDto.java
@@ -0,0 +1,79 @@
+package com.rafaeldebiase.taskmanager.dto;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+public class TarefaNewDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String titulo;
+	private String descricao;
+	private Boolean concluido;
+	private Calendar dataCriacao;
+	
+	private Integer idUsuario;
+	private String nome;
+	private String email;
+	
+	public TarefaNewDto() {
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Boolean getConcluido() {
+		return concluido;
+	}
+
+	public void setConcluido(Boolean concluido) {
+		this.concluido = concluido;
+	}
+
+	public Calendar getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Calendar dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+
+}
