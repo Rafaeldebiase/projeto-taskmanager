5865db0586a97caf1adda75b1c7ed4ce6911ca0b
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-01-21 00:01:37 -0200

    implementando o swagger

--- src/main/java/com/rafaeldebiase/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/rafaeldebiase/taskmanager/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -45,6 +46,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = { "/tarefas/**", "/usuarios/**" };
 
 	private static final String[] PUBLIC_MATCHERS_POST = { "/usuarios/**" };
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		 web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
+		"/swagger-ui.html", "/webjars/**");
+	}
 
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable();
